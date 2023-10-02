d42b8b4d6d98d35fad796bdaa1cdc24aa9576b99
Philippos123 <>
2023-10-02 14:07:02 +0000

    Add validation to check for exactly 6 values in provided data

--- run.py
+++ run.py
@@ -23,6 +23,18 @@ def get_sales_data():
     data_str = input("Enter your data here")
     
     sales_data = data_str.split(",")
-    print(sales_data)
+    validate_data(sales_data)
 
+
+def validate_data(values):
+    print(values)
+
+    try:
+        if len(values) !=6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+
+            )
+    except ValueError as e:
+        print(f"Invalid data {e}, please try again.\n")
 get_sales_data()
