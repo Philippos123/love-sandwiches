36aae2a016173b58781c4343ed7f77280e4e6b76
Philippos123 <>
2023-10-02 15:31:33 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ def get_sales_data():
     while True:
         print("Please enter sales data from the last market")
         print("Data should be six numbers, separated by commas")
-        print("Exampel: 10,20,30,40,50,60")
+        print("Exampel: 10,20,30,40,50,60"\n)
 
         data_str = input("Enter your data here")
     
@@ -79,7 +79,14 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+
+
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    return surplus_data
+
 
 
 def main():
@@ -89,8 +96,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
-
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 
 print("Welcome to love Sandwiches Data Automation")
